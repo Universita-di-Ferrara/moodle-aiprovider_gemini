d397c242fca52a9d49a7f4f9c6118068ae3090a1
BertelliAndrea <berteandrea@gmail.com>
2025-12-09 10:25:47 +0000

    version update

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_gemini';
-$plugin->release = '1.0.1';
-$plugin->version = 2025092902;
+$plugin->release = '2.0.0';
+$plugin->version = 2025120900;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_STABLE;
